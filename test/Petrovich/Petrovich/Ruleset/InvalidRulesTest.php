fdaa6fccdda1f354d793fe6f1515ea0b27b225f4
staticall <staticall@gmail.com>
2019-05-18 05:58:39 +0700

    Should be remaining coverage for validation method

--- test/Petrovich/Petrovich/Ruleset/InvalidRulesTest.php
+++ test/Petrovich/Petrovich/Ruleset/InvalidRulesTest.php
@@ -88,6 +88,106 @@ class InvalidRulesTest extends TestCase
         $ruleset->setRules($rules, true);
     }
 
+    public function testSetInvalidValueKeyTestNotArrayThroughConstruct()
+    {
+        $rules = $this->getRulesValueTestNotArray();
+
+        $this->shouldExpectValidationException();
+
+        new Ruleset($rules, true);
+    }
+
+    public function testSetInvalidValueKeyTestNotArrayThroughSetter()
+    {
+        $rules = $this->getRulesValueTestNotArray();
+
+        $this->shouldExpectValidationException();
+
+        $ruleset = new Ruleset([], false);
+
+        $ruleset->setRules($rules, true);
+    }
+
+    public function testSetInvalidValueKeyModsNotArrayThroughConstruct()
+    {
+        $rules = $this->getRulesValueModsNotArray();
+
+        $this->shouldExpectValidationException();
+
+        new Ruleset($rules, true);
+    }
+
+    public function testSetInvalidValueKeyModsNotArrayThroughSetter()
+    {
+        $rules = $this->getRulesValueModsNotArray();
+
+        $this->shouldExpectValidationException();
+
+        $ruleset = new Ruleset([], false);
+
+        $ruleset->setRules($rules, true);
+    }
+
+    public function testSetInvalidValueKeyTagsNotArrayThroughConstruct()
+    {
+        $rules = $this->getRulesValueTagsNotArray();
+
+        $this->shouldExpectValidationException();
+
+        new Ruleset($rules, true);
+    }
+
+    public function testSetInvalidValueKeyTagsNotArrayThroughSetter()
+    {
+        $rules = $this->getRulesValueTagsNotArray();
+
+        $this->shouldExpectValidationException();
+
+        $ruleset = new Ruleset([], false);
+
+        $ruleset->setRules($rules, true);
+    }
+
+    public function testSetInvalidValueKeyGenderNotStringThroughConstruct()
+    {
+        $rules = $this->getRulesValueGenderNotString();
+
+        $this->shouldExpectValidationException();
+
+        new Ruleset($rules, true);
+    }
+
+    public function testSetInvalidValueKeyGenderNotStringThroughSetter()
+    {
+        $rules = $this->getRulesValueGenderNotString();
+
+        $this->shouldExpectValidationException();
+
+        $ruleset = new Ruleset([], false);
+
+        $ruleset->setRules($rules, true);
+    }
+
+    public function testSetInvalidValueKeyGenderInvalidThroughConstruct()
+    {
+        $rules = $this->getRulesValueGenderInvalid();
+
+        $this->shouldExpectValidationException();
+
+        new Ruleset($rules, true);
+    }
+
+    public function testSetInvalidValueKeyGenderInvalidThroughSetter()
+    {
+        $rules = $this->getRulesValueGenderInvalid();
+
+        $this->shouldExpectValidationException();
+
+        $ruleset = new Ruleset([], false);
+
+        $ruleset->setRules($rules, true);
+    }
+
     public function getRulesRoot()
     {
         return [
@@ -122,6 +222,71 @@ class InvalidRulesTest extends TestCase
                 Ruleset::SECOND_KEY_SUFFIXES => [
                     [
                         'unknownValue' => [],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function getRulesValueTestNotArray()
+    {
+        return [
+            Ruleset::ROOT_KEY_FIRSTNAME => [
+                Ruleset::SECOND_KEY_SUFFIXES => [
+                    [
+                        Ruleset::VALUE_KEY_TEST => 'test',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function getRulesValueModsNotArray()
+    {
+        return [
+            Ruleset::ROOT_KEY_FIRSTNAME => [
+                Ruleset::SECOND_KEY_SUFFIXES => [
+                    [
+                        Ruleset::VALUE_KEY_MODS => 'mods',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function getRulesValueTagsNotArray()
+    {
+        return [
+            Ruleset::ROOT_KEY_FIRSTNAME => [
+                Ruleset::SECOND_KEY_SUFFIXES => [
+                    [
+                        Ruleset::VALUE_KEY_TAGS => 'tags',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function getRulesValueGenderNotString()
+    {
+        return [
+            Ruleset::ROOT_KEY_FIRSTNAME => [
+                Ruleset::SECOND_KEY_SUFFIXES => [
+                    [
+                        Ruleset::VALUE_KEY_GENDER => [],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function getRulesValueGenderInvalid()
+    {
+        return [
+            Ruleset::ROOT_KEY_FIRSTNAME => [
+                Ruleset::SECOND_KEY_SUFFIXES => [
+                    [
+                        Ruleset::VALUE_KEY_GENDER => 'unknownGender',
                     ],
                 ],
             ],
